db74e73c85fb0bda70bb9789915cedb4b58992e4
Omer <omerbnb@gmail.com>
2023-05-22 14:52:22 +0300

    login

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,13 +1,19 @@
 const authService = require('./auth.service')
 const logger = require('../../services/logger.service')
 
+module.exports = {
+    login,
+    signup,
+    logout
+}
+
 async function login(req, res) {
     const { username, password } = req.body
     try {
         const user = await authService.login(username, password)
         const loginToken = authService.getLoginToken(user)
         logger.info('User login: ', user)
-        res.cookie('loginToken', loginToken, {sameSite: 'None', secure: true})
+        res.cookie('loginToken', loginToken, { sameSite: 'None', secure: true })
         res.json(user)
     } catch (err) {
         logger.error('Failed to Login ' + err)
@@ -18,14 +24,16 @@ async function login(req, res) {
 async function signup(req, res) {
     try {
         const credentials = req.body
-        // Never log passwords
+        const password = req.body.password
         // logger.debug(credentials)
         const account = await authService.signup(credentials)
         logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
-        const user = await authService.login(credentials.username, credentials.password)
+
+        const user = await authService.login(credentials.username, password)
         logger.info('User signup:', user)
+
         const loginToken = authService.getLoginToken(user)
-        res.cookie('loginToken', loginToken, {sameSite: 'None', secure: true})
+        res.cookie('loginToken', loginToken, { sameSite: 'None', secure: true })
         res.json(user)
     } catch (err) {
         logger.error('Failed to signup ' + err)
@@ -33,17 +41,11 @@ async function signup(req, res) {
     }
 }
 
-async function logout(req, res){
+async function logout(req, res) {
     try {
         res.clearCookie('loginToken')
         res.send({ msg: 'Logged out successfully' })
     } catch (err) {
         res.status(500).send({ err: 'Failed to logout' })
     }
-}
-
-module.exports = {
-    login,
-    signup,
-    logout
 }
